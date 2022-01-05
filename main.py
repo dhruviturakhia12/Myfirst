4f1a0c72d30afa861a22d77f4046ffb8a93e28c0
aubergine <dhruvi.t@auberginesolutions.com>
2022-01-05 17:22:28 +0530

    done

--- main.py
+++ main.py
@@ -5,17 +5,8 @@ if ip==("Dhruvi" and "dhruvi"):
     print("Change your name please")
     print("Thanks")
 else:
-<<<<<<< HEAD
-<<<<<<< HEAD
-    print("Nice name")
-    print("Bye")
-=======
     new=input("Are you Soham")
     if new=="yes":
         print("shit")
     else:
         print("Okay!")
->>>>>>> new
-=======
-    print("Nice name")
->>>>>>> parent of 04ab340... branch
